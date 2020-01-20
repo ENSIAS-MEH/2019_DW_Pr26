630c89826df3c8abf9bec27266c72792a05af801
GHAZLANE MOHAMMED <med.ghazlane@gmail.com>
2020-01-20 02:09:25 +0100

    optimistation get Action

--- src/web/Controller.java
+++ src/web/Controller.java
@@ -11,38 +11,39 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class Controller
  */
 
-@WebServlet( name="Controller", urlPatterns = {"/", "/Controller.ma"} )
+@WebServlet(name = "Controller", urlPatterns = { "/", "/Controller.ma" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    
-    public Controller() {
-        // TODO Auto-generated constructor stub
-    }
-
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String views = "Accueil"; 
-		String action = getActionKey(request); 
-		System.out.println(action);
-		request.getRequestDispatcher(views+".jsp").forward(request, response);
+	public Controller() {
+		// TODO Auto-generated constructor stub
 	}
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String views = "Accueil";
+		String action = getActionKey(request);
+		if (action.equals(""))
+			views = "Accueil";
+		else
+			views = "/404";
+
 	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher(views + ".jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-	
+
 	// recuperer le type de la demande de l'utilisateur
 	private String getActionKey(HttpServletRequest request) {
 		String uri = request.getRequestURI();
-		int i = uri.lastIndexOf("/");
-		if(uri.contains(".ma")){
-			int j = uri.lastIndexOf(".ma");
-		return uri.substring(i + 1, j);
-		}
-		else return uri.substring(i + 1, uri.length()); 
+		if (uri.contains(".ma"))
+			return uri.substring(uri.lastIndexOf("/") + 1, uri.lastIndexOf(".ma"));
+		else
+			return uri.substring(uri.lastIndexOf("/") + 1, uri.length());
 	}
-
 }
