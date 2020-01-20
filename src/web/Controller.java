e1576c4f996f9644ea401540291be6d430b3a6b4
GHAZLANE MOHAMMED <med.ghazlane@gmail.com>
2020-01-20 01:36:00 +0100

    get Action

--- src/web/Controller.java
+++ src/web/Controller.java
@@ -10,32 +10,39 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Controller
  */
-@WebServlet("/Controller")
+
+@WebServlet( name="Controller", urlPatterns = {"/", "/Controller.ma"} )
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
+    
     public Controller() {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		request.getRequestDispatcher("FileTestPlateForme.jsp").forward(request, response);
+		String views = "Accueil"; 
+		String action = getActionKey(request); 
+		System.out.println(action);
+		request.getRequestDispatcher(views+".jsp").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+	}
+	
+	// recuperer le type de la demande de l'utilisateur
+	private String getActionKey(HttpServletRequest request) {
+		String uri = request.getRequestURI();
+		int i = uri.lastIndexOf("/");
+		if(uri.contains(".ma")){
+			int j = uri.lastIndexOf(".ma");
+		return uri.substring(i + 1, j);
+		}
+		else return uri.substring(i + 1, uri.length()); 
 	}
 
 }
